c009ebd048a39ee2972e289eb53cb6258629e4aa
Madagambada <47931256+Madagambada@users.noreply.github.com>
2023-10-13 20:11:01 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 echo -n "Set toolchain vars... "
-export AR=/usr/bin/llvm-ar-16
-export AS=/usr/bin/llvm-as-16
-export CC=/usr/bin/clang-16
-export CXX=/usr/bin/clang++-16
-export LD=/usr/bin/llvm-ld-16
-export RANLIB=/usr/bin/llvm-ranlib-16
-export STRIP=/usr/bin/llvm-strip-16
+export AR=/usr/bin/llvm-ar-17
+export AS=/usr/bin/llvm-as-17
+export CC=/usr/bin/clang-17
+export CXX=/usr/bin/clang++-17
+export LD=/usr/bin/llvm-ld-17
+export RANLIB=/usr/bin/llvm-ranlib-17
+export STRIP=/usr/bin/llvm-strip-17
 
 echo -n "Create build directory... "
 mkdir build
